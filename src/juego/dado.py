9b650a8d34be38a9bc8de042ca6a2e012e691d00
marCRACK29 <mamartinez2023@udec.cl>
2025-08-23 18:33:37 -0400

    REFACTOR: mejora método de generación con dependency injection. Además de nombres más apropiados y atributos privados

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -1,10 +1,9 @@
-import random
-
 class Dado: 
-    def __init__(self):
-        self.numero = None
+    def __init__(self, generador_numeros):
+        self._generador = generador_numeros
+        self._ultimo_resultado = None
     
-    def tirar_dado(self) -> int:
-        self.numero = random.randint(1, 6)
-        return self.numero
+    def tirar(self) -> int:
+        self._ultimo_resultado = self._generador(1,6)
+        return self._ultimo_resultado
         
--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,12 +1,13 @@
 import pytest
 from src.juego.dado import Dado
 
-def test_tirar_dado(mocker):
-    numero_esperado = 3
-    mocker.patch('src.juego.dado.random.randint', return_value=numero_esperado)
+# Ejecutar test 3 veces con 3 valores (1, 3, y 6)
+@pytest.mark.parametrize("numero_esperado", [1, 3, 6])
+def test_tirar_dado(mocker, numero_esperado):
+    mock_prueba = mocker.Mock(return_value=numero_esperado)
 
-    dado_prueba = Dado()
-    resultado = dado_prueba.tirar_dado()
+    dado_prueba = Dado(mock_prueba)
+    resultado = dado_prueba.tirar()
 
     assert resultado == numero_esperado
-
+    mock_prueba.assert_called_once_with(1, 6)
