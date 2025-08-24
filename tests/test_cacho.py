13cb9340539bf9f5d9132cf300464372b84cd62e
marCRACK29 <mamartinez2023@udec.cl>
2025-08-24 13:41:39 -0400

    RED: test para retornar los resultados de un cacho - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -9,3 +9,21 @@ def test_agitar(mocker):
 
     assert mensaje == "Cacho agitado!"
     assert mock_tirar.call_count == 5 #Para verificar que se tiraron 5 dados
+
+def test_resultados(mocker):
+    resultados_esperados = [1, 2, 3, 4, 5]
+    MockDado = mocker.patch('src.juego.cacho.Dado')
+    dados_mock = []
+
+    for numero in resultados_esperados:
+        dado_mock = mocker.Mock()
+        dado_mock.tirar.return_value = numero
+        dados_mock.append(dado_mock)
+    
+    MockDado.side_effect = dados_mock
+
+    cacho_prueba = Cacho()
+    valores_obtenidos = cacho_prueba.resultados()
+    assert valores_obtenidos == resultados_esperados
+
+       
