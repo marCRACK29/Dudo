da123f2c97c39c1a69c6df40fb75863ab1664f73
marCRACK29 <mamartinez2023@udec.cl>
2025-08-23 20:41:46 -0400

    RED: test para obtener el último número obtenido del azar - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -12,6 +12,7 @@ def test_tirar_dado(mocker, numero_esperado):
     assert resultado == numero_esperado
     mock_prueba.assert_called_once_with(1, 6)
 
+# Ejecución de test 6 veces para analizar todas las pintas
 @pytest.mark.parametrize("numero, pinta_esperada", [
     (1, "as"),
     (2, "tonto"), 
@@ -29,3 +30,17 @@ def test_get_pinta(mocker, numero, pinta_esperada):
 
     assert resultado == pinta_esperada
 
+@pytest.mark.parametrize("numero_esperado", [2, 4, 6])
+def test_ultimo_resultado(mocker, numero_esperado):
+    mock_prueba = mocker.Mock(return_value=numero_esperado)
+    dado_prueba = Dado(mock_prueba)
+
+    dado_prueba.tirar()
+    # el valor debe cambiar solo con el método tirar(), get solo debe retornar el último valor
+    # hacemos una corta simulación de ello llamando al método 3 veces 
+    # (y asi verifcar que no cambie el valor). 
+    dado_prueba.ultimo_resultado()
+    dado_prueba.ultimo_resultado()
+    resultado = dado_prueba.ultimo_resultado()
+
+    assert resultado == numero_esperado
