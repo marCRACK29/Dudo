d8570985dc2cde5cc99c2fe03431db04de1e134b
Karantooo <ctomasalvarezn@gmail.com>
2025-08-25 14:07:47 -0400

    GREEN: se implementa excepcion para controlar que rotacion sea una Rotacion

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -10,6 +10,8 @@ class ArbitroRonda:
             raise ValueError("Jugador inicial negativo")
         if primer_jugador_id >= cantidad_jugadores:
             raise ValueError("Jugador inicial superior a cantidad de jugadores")
+        if not isinstance(rotacion, Rotacion):
+            raise ValueError("Valor dado no es una Rotacion")
 
         self.cantidad_jugadores = cantidad_jugadores
         self.jugador_actual_id = primer_jugador_id
