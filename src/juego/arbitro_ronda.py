e19e927a03d9348285af54aa58c960228760bf94
Karantooo <ctomasalvarezn@gmail.com>
2025-08-25 02:17:17 -0400

    RED: test para verificar la rotacion en sentido antihorario - falla como esperado

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -1,5 +1,11 @@
+from enum import Enum
+
+class Rotacion(Enum):
+    HORARIO = 1
+    ANTIHORARIO = -1
+
 class ArbitroRonda:
-    def __init__(self, first_player_id, cantidad_jugadores):
+    def __init__(self, first_player_id, cantidad_jugadores, rotacion=Rotacion.HORARIO):
         self.cantidad_jugadores = cantidad_jugadores
         self.jugador_actual_id = first_player_id
 
--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -1,5 +1,5 @@
 import pytest
-from src.juego.arbitro_ronda import ArbitroRonda
+from src.juego.arbitro_ronda import ArbitroRonda, Rotacion
 
 
 @pytest.mark.parametrize(
@@ -10,9 +10,24 @@ from src.juego.arbitro_ronda import ArbitroRonda
         (4, 6, 5),
         (1, 2, 0)
     ],
-    ids=["empieza_1","empieza_3","empieza_4", "empieza_2_y_da_vuelta"]
+    ids=["empieza_1","empieza_3","empieza_4", "empieza_1_y_da_vuelta"]
 )
 def test_rotacion_jugadores_horaria(inicial,cantidad, esperado):
     arbitro = ArbitroRonda(inicial, cantidad)
+    arbitro.siguiente_jugador()
+    assert arbitro.jugador_actual_id == esperado
+
+@pytest.mark.parametrize(
+    "inicial, cantidad, esperado",
+    [
+        (1, 6, 0),
+        (0, 5, 4),
+        (4, 6, 3),
+        (0, 2, 1)
+    ],
+    ids=["empieza_1","empieza_3","empieza_4", "empieza_0_y_da_vuelta"]
+)
+def test_rotacion_jugadores_antihoraria(inicial,cantidad, esperado):
+    arbitro = ArbitroRonda(inicial, cantidad, Rotacion.ANTIHORARIO)
     arbitro.siguiente_jugador()
     assert arbitro.jugador_actual_id == esperado
