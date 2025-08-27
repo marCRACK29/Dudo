2730e7d3c79712e243887f0d0760a07b6ced4f0c
VicenteRamirez15 <vramirezt15@gmail.com>
2025-08-26 20:09:02 -0400

    GREEN: implementación de metodo para retornar la cantidad de dados de un jugador

--- src/juego/jugador.py
+++ src/juego/jugador.py
@@ -5,3 +5,7 @@ class Jugador:
     def __init__(self):
         self.cacho = Cacho()
         self.dados = [Dado() for _ in range(5)]
+        self.cantidad_dados = len(self.dados)
+
+    def total_de_dados(self):
+        return self.cantidad_dados
