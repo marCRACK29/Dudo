d6f4249ff167ba96ee0a27e34aa469ab38fe465f
Karantooo <ctomasalvarezn@gmail.com>
2025-08-27 23:10:18 -0400

    GREEN: se implementan los jugadores dentro de gestor partida

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -1,5 +1,7 @@
+from src.juego.jugador import Jugador
+
 class GestorPartida:
 
     def __init__(self, cantidad_jugadores):
-        self.jugadores = []
+        self.jugadores = [Jugador() for i in range(cantidad_jugadores)]
 
