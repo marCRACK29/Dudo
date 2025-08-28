4bcbb864de631c2e245d3bdbe6ee87b490786abd
Karantooo <ctomasalvarezn@gmail.com>
2025-08-28 00:25:51 -0400

    test: se añaden casos limites para el tdd que faltaron en commit anterior

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -13,14 +13,20 @@ def test_creacion_jugadores(cantidad_jugadores):
     for jugador in jugadores:
         assert isinstance(jugador, Jugador)
 
-
-def test_seis_jugadores_lanzan_controlado(mocker):
+@pytest.mark.parametrize("datos_lanzamientos, resultado",
+    [
+        ([1, 2, 3, 4, 5, 6], 5),
+        ([1, 2, 3, 4, 6, 6, 6, 1], 4),
+        ([6, 6, 6, 6, 6, 6, 1, 1, 6, 1, 6, 1, 3, 5], 5)
+    ]
+)
+def test_seis_jugadores_lanzan_controlado(mocker, datos_lanzamientos, resultado):
     # Parchea Dado EN EL MÓDULO donde se usa (jugador.py)
     MockDado = mocker.patch('src.juego.jugador.Jugador')
     # Cada llamada a .tirar() devolverá, en orden: 1,2,3,4,5,6
-    MockDado.return_value.tirar.side_effect = [1, 2, 3, 4, 5, 6]
+    MockDado.return_value.tirar.side_effect = datos_lanzamientos
 
     partida = GestorPartida(6)
 
 
-    assert partida.primer_jugador == 5
+    assert partida.primer_jugador == resultado
